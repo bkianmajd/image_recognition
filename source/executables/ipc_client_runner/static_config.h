6ba5d1b0e31e29a5c7c49c482de13bf20e3bea17
babak <bbkianmajd@ucdavis.edu>
2018-12-22 10:28:29 -0800

    renamed to protobuf

--- source/executables/ipc_client_runner/static_config.h
+++ source/executables/ipc_client_runner/static_config.h
@@ -1,8 +1,10 @@
+#include <chrono>
+
 namespace ipc_client {
 
 constexpr int kPort = 8080;
 // const char* kConnectionAddress = "123.123.123";
-const char* kConnectionAddress = "127.0.0.1";
+constexpr char kConnectionAddress[] = "127.0.0.1";
 const std::chrono::seconds kTimeout = std::chrono::seconds(5);
 
 }  // ipc_client
