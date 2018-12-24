5febd3f3837300b9b25b3925759c38de6d5c4844
babak <bbkianmajd@ucdavis.edu>
2018-12-24 09:51:18 -0800

    Changed local ip address

--- source/executables/ipc_client_runner/static_config.h
+++ source/executables/ipc_client_runner/static_config.h
@@ -3,8 +3,8 @@
 namespace ipc_client {
 
 constexpr int kPort = 8080;
-// const char* kConnectionAddress = "123.123.123";
-constexpr char kConnectionAddress[] = "127.0.0.1";
+constexpr char kConnectionAddress[] = "192.168.86.24";
+// constexpr char kConnectionAddress[] = "127.0.0.1";
 const std::chrono::seconds kTimeout = std::chrono::seconds(5);
 
 }  // ipc_client
